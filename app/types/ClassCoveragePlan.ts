01893de3d00bdece281c0a70fbf4aaffa3e5f92b
AkaPetrovic <akapetrovic@gmail.com>
2024-08-05 20:48:07 +0200

    Made id optional for the ClassCoveragePlan type interface

--- app/types/ClassCoveragePlan.ts
+++ app/types/ClassCoveragePlan.ts
@@ -3,7 +3,7 @@ import Subject from "./Subject";
 import AcademicYear from "./AcademicYear";
 
 export default interface ClassCoveragePlan {
-  id: number;
+  id?: number;
   amountOfLectureClassesPerTeachingStaff: number;
   amountOfPracticalClassesPerTeachingStaff: number;
   amountOfLabPracticalClassesPerTeachingStaff: number;
