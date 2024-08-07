bce43c757517195afc200f3ea6519def4390d986
AkaPetrovic <akapetrovic@gmail.com>
2024-08-07 14:37:20 +0200

    Created a page for listing all the subjects

--- app/subjects/page.tsx
+++ app/subjects/page.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Link from "next/link";
+import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
+import Subject from "../types/Subject";
 
-const SubjectsPage = () => {
-  return <div>SubjectsPage</div>;
+const SubjectsPage = async () => {
+  const res = await fetch("http://localhost:8080/api/subjects", {
+    cache: "no-store",
+  });
+  const subjects: Subject[] = await res.json();
+
+  return (
+    <main className="flex justify-center items-center relative w-full h-full">
+      <Link
+        href="/"
+        className="btn btn-ghost rounded-full absolute top-4 left-6 p-3"
+      >
+        <ArrowBackIosNewRoundedIcon />
+      </Link>
+      <div className="w-3/5">
+        <h1 className="mb-5">Subjects</h1>
+        <table className="table table-lg">
+          <thead>
+            <tr className="border-b border-base-300 text-base">
+              <th>ID</th>
+              <th>Name</th>
+              <th>ESPB</th>
+            </tr>
+          </thead>
+          <tbody className="[&_tr:not(:last-child)]:border-b [&_tr:not(:last-child)]:border-base-300">
+            {subjects.map((subject: Subject) => (
+              <tr
+                className="hover:bg-base-200 transition-colors duration-300"
+                key={subject.id}
+              >
+                <td>{subject.id}</td>
+                <td>{subject.name}</td>
+                <td>{subject.espb}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
 };
 
 export default SubjectsPage;
